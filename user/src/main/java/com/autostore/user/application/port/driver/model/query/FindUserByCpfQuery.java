525ed5e084163cf492a73aef505e4fd147052d20
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-08-29 20:23:44 -0300

    feat: FindUserByCpfDriverPort

---
+++ user/src/main/java/com/autostore/user/application/port/driver/model/query/FindUserByCpfQuery.java
@@ -0,0 +1,6 @@
+package com.autostore.user.application.port.driver.model.query;
+
+
+public record FindUserByCpfQuery(String cpf) {
+
+}
---
+++ user/src/main/java/com/autostore/user/application/port/driver/model/query/FindUserByCpfQueryOutput.java
@@ -0,0 +1,9 @@
+package com.autostore.user.application.port.driver.model.query;
+
+
+import com.autostore.user.domain.User;
+
+
+public record FindUserByCpfQueryOutput(User user) {
+
+}
