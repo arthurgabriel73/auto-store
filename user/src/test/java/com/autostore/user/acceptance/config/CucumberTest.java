6be5464d6fcd914e6f2e53491a47654853db1af6
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-08-30 14:30:08 -0300

    refactor: refactors annotations

--- user/src/test/java/com/autostore/user/acceptance/config/CucumberTest.java
+++ user/src/test/java/com/autostore/user/acceptance/config/CucumberTest.java
@@ -9,7 +9,6 @@ import org.junit.platform.suite.api.Suite;
 
 import java.io.File;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -18,9 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "usage")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-reports.html")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.javastaff.bddfun.test.glue")
 class CucumberTest {
 
     @Test
