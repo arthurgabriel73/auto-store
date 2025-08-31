59dcaba3c59c8af681e26f2eea677c630153518f
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-08-31 00:45:19 -0300

    feat: DuplicatedActivityException

---
+++ inventory/src/main/java/com/autostore/inventory/application/exception/DuplicatedActivityException.java
@@ -0,0 +1,10 @@
+package com.autostore.inventory.application.exception;
+
+
+public class DuplicatedActivityException extends ApplicationException {
+
+    public DuplicatedActivityException(String message) {
+        super(message);
+    }
+
+}
