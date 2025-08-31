e18dc58b36ca25b7817f0cc68e244a2fbfd515bc
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-08-31 11:43:34 -0300

    feat: InventoryNotFoundException

---
+++ inventory/src/main/java/com/autostore/inventory/application/exception/InventoryNotFoundException.java
@@ -0,0 +1,10 @@
+package com.autostore.inventory.application.exception;
+
+
+public class InventoryNotFoundException extends ApplicationException {
+
+    public InventoryNotFoundException(String message) {
+        super(message);
+    }
+
+}
