d831a30ab8d853b77bb678558febd32174b3b6e0
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-08-31 20:29:00 -0300

    feat: domain exceptions

---
+++ payment/src/main/java/com/autostore/payment/domain/exceptions/ValidationException.java
@@ -0,0 +1,10 @@
+package com.autostore.payment.domain.exceptions;
+
+
+public class ValidationException extends BusinessException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+}
