67cd09ae9fb33b34d5a626a93735eb0704959f3e
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-09-01 18:10:26 -0300

    feat: implements UpdateProductRequest

--- bff/src/main/java/com/autostore/bff/application/service/inventory/dto/UpdateProductRequest.java
+++ bff/src/main/java/com/autostore/bff/application/service/inventory/dto/UpdateProductRequest.java
@@ -1,6 +1,19 @@
 package com.autostore.bff.application.service.inventory.dto;
 
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Map;
+
+
+@Data
+@Builder
 public class UpdateProductRequest {
+
+    String code;
+    Double unitValue;
+    String category;
+    Map<String, Object> details;
 
 }
