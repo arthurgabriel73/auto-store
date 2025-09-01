2edb68759803756bdf5bda8c2a827d950ce0a00a
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-09-01 01:55:36 -0300

    feat: JsonUtil

---
+++ inventory/src/main/java/com/autostore/inventory/infrastructure/adapter/util/JsonUtil.java
@@ -0,0 +1,39 @@
+package com.autostore.inventory.infrastructure.adapter.util;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class JsonUtil {
+
+    private final ObjectMapper objectMapper;
+
+    public String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+            return "";
+        }
+    }
+
+    public <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(json, clazz);
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    public Object fromJson(String json) {
+        return fromJson(json, Object.class);
+    }
+
+}
