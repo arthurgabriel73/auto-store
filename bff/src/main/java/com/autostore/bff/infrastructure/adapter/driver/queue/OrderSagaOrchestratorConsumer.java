d6bd1290eeaa988d842afeea303503463601b8ff
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-09-02 01:31:51 -0300

    refactor: configure single consumer for all Order events

---
+++ bff/src/main/java/com/autostore/bff/infrastructure/adapter/driver/queue/OrderSagaOrchestratorConsumer.java
@@ -0,0 +1,35 @@
+package com.autostore.bff.infrastructure.adapter.driver.queue;
+
+
+import com.autostore.bff.application.port.driver.OrderOrchestratorServiceDriverPort;
+import com.autostore.bff.domain.DomainEvent;
+import com.autostore.bff.domain.Topic;
+import com.autostore.bff.domain.order.Order;
+import com.autostore.bff.infrastructure.adapter.util.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class OrderSagaOrchestratorConsumer {
+
+    private final OrderOrchestratorServiceDriverPort orderOrchestratorServiceDriverPort;
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "#{@orderSagaTopics}"
+    )
+    public void consumeOrderSagaEvent(String payload, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        log.info("Receiving event from topic {}: {}", topic, payload);
+        DomainEvent<Order> event = jsonUtil.fromJson(payload, DomainEvent.class);
+        orderOrchestratorServiceDriverPort.consumeOrderSagaEvent(event, Topic.fromString(topic));
+    }
+
+}
