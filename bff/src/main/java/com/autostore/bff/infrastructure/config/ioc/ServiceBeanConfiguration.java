ffc59293558852745f9a59f1a3114e23a0c2e871
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-09-01 22:48:11 -0300

    feat: ioc OrchestratorServiceDriverPort

--- bff/src/main/java/com/autostore/bff/infrastructure/config/ioc/ServiceBeanConfiguration.java
+++ bff/src/main/java/com/autostore/bff/infrastructure/config/ioc/ServiceBeanConfiguration.java
@@ -1,9 +1,13 @@
 package com.autostore.bff.infrastructure.config.ioc;
 
 
+import com.autostore.bff.application.port.driven.EventProducer;
 import com.autostore.bff.application.port.driven.InventoryGateway;
 import com.autostore.bff.application.port.driven.OrderGateway;
 import com.autostore.bff.application.port.driven.UserGateway;
+import com.autostore.bff.application.port.driver.OrchestratorServiceDriverPort;
+import com.autostore.bff.application.service.OrchestratorService;
+import com.autostore.bff.application.service.SagaExecutionController;
 import com.autostore.bff.application.service.inventory.InventoryService;
 import com.autostore.bff.application.service.order.OrderService;
 import com.autostore.bff.application.service.user.UserService;
@@ -27,6 +31,13 @@ public class ServiceBeanConfiguration {
     @Bean
     public OrderService orderService(OrderGateway orderGateway) {
         return new OrderService(orderGateway);
+    }
+
+    @Bean
+    OrchestratorServiceDriverPort orchestratorServiceDriverPort(
+            EventProducer eventProducer
+    ) {
+        return new OrchestratorService(new SagaExecutionController(), eventProducer);
     }
 
 }
