9884f99cce03a0e572b19f519c941941eed18e58
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-09-01 21:08:17 -0300

    feat: api examples

--- bff/src/main/java/com/autostore/bff/infrastructure/adapter/driver/rest/AutoStoreController.java
+++ bff/src/main/java/com/autostore/bff/infrastructure/adapter/driver/rest/AutoStoreController.java
@@ -11,17 +11,20 @@ import com.autostore.bff.application.service.user.dto.AuthUserRequest;
 import com.autostore.bff.application.service.user.dto.AuthUserResponse;
 import com.autostore.bff.application.service.user.dto.RegisterUserRequest;
 import com.autostore.bff.application.service.user.dto.RegisterUserResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.autostore.bff.infrastructure.adapter.driver.rest.examples.SwaggerExamples.*;
+
 
 @RestController
 @RequestMapping("/auto-store")
 @RequiredArgsConstructor
-@Tag(name = "auto-store-api")
 public class AutoStoreController {
 
     private final UserService userService;
@@ -29,44 +32,74 @@ public class AutoStoreController {
     private final OrderService orderService;
 
     @PostMapping("/auth")
-    public ResponseEntity<AuthUserResponse> auth(@RequestBody AuthUserRequest request) {
+    @Tag(name = "Customer")
+    public ResponseEntity<AuthUserResponse> auth(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(examples = @ExampleObject(value = AUTH_USER)))
+            @RequestBody AuthUserRequest request
+    ) {
         return ResponseEntity.ok(userService.authUser(request));
     }
 
     @PostMapping("/register-customer")
-    public ResponseEntity<RegisterUserResponse> registerUser(@RequestBody RegisterUserRequest request) {
+    @Tag(name = "Customer")
+    public ResponseEntity<RegisterUserResponse> registerUser(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(examples = @ExampleObject(value
+                    = REGISTER_USER)))
+            @RequestBody RegisterUserRequest request
+    ) {
         return new ResponseEntity<>(userService.registerUser(request), HttpStatus.CREATED);
     }
 
     @PostMapping("/register-product")
-    public ResponseEntity<ProductResponse> registerProduct(@RequestBody RegisterProductRequest request) {
+    @Tag(name = "Product")
+    public ResponseEntity<ProductResponse> registerProduct(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(examples = @ExampleObject(value
+                    = REGISTER_PRODUCT)))
+            @RequestBody RegisterProductRequest request
+    ) {
         return new ResponseEntity<>(inventoryService.registerProduct(request), HttpStatus.CREATED);
     }
 
     @PostMapping("/add-product-stock")
-    public ResponseEntity<Void> registerProduct(@RequestBody AddProductStockRequest request) {
+    @Tag(name = "Product")
+    public ResponseEntity<Void> addProductStock(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(examples = @ExampleObject(value
+                    = ADD_PRODUCT_STOCK)))
+            @RequestBody AddProductStockRequest request) {
         inventoryService.addProductStock(request);
         return ResponseEntity.accepted().build();
     }
 
     @GetMapping("/available-products")
+    @Tag(name = "Product")
     public ResponseEntity<ListAvailableProductsResponse> listAvailableProducts() {
         return ResponseEntity.ok(inventoryService.listAvailableProducts());
     }
 
     @GetMapping("/sold-products")
+    @Tag(name = "Product")
     public ResponseEntity<ListSoldProductsResponse> listSoldProducts() {
         return ResponseEntity.ok(inventoryService.listSoldProducts());
     }
 
     @PutMapping("/update-product")
-    public ResponseEntity<Void> updateProduct(@RequestBody UpdateProductRequest request) {
+    @Tag(name = "Product")
+    public ResponseEntity<Void> updateProduct(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(examples = @ExampleObject(value
+                    = UPDATE_PRODUCT)))
+            @RequestBody UpdateProductRequest request
+    ) {
         inventoryService.updateProduct(request);
         return ResponseEntity.accepted().build();
     }
 
     @PostMapping("/create-order")
-    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderRequest request) {
+    @Tag(name = "Order")
+    public ResponseEntity<CreateOrderResponse> createOrder(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(examples = @ExampleObject(value
+                    = CREATE_ORDER)))
+            @RequestBody CreateOrderRequest request) {
+        // TODO: Get customer from auth token provided in the request header
         return new ResponseEntity<>(orderService.createOrder(request), HttpStatus.CREATED);
     }
 
---
+++ bff/src/main/java/com/autostore/bff/infrastructure/adapter/driver/rest/examples/SwaggerExamples.java
@@ -0,0 +1,87 @@
+package com.autostore.bff.infrastructure.adapter.driver.rest.examples;
+
+
+public class SwaggerExamples {
+
+    public static final String AUTH_USER = """
+            {
+                "cpf": "636.803.004-68"
+            }
+            """;
+
+    public static final String REGISTER_USER = """
+            {
+            	"firstName": "Nair",
+            	"lastName": "Moreira",
+            	"cpf": "636.803.004-68",
+            	"email": "nair_moreira@suplementototal.com.br",
+            	"street": "Rua Palomas",
+            	"number": "330",
+            	"neighborhood": "Jardim das Palmeiras",
+            	"city": "Ariquemes",
+            	"state": "RO",
+            	"zipCode": "76876-642",
+            	"complement": "CASA",
+            	"country": "Brasil"
+            }
+            """;
+
+    public static final String REGISTER_PRODUCT = """
+                {
+                  "code": "MYW-6552",
+                  "unitValue": 686670.00,
+                  "category": "VEHICLE",
+                  "details": {
+                    "plate": "MYW-6552",
+                    "make": "ASTON MARTIN",
+                    "model": "DB9 Volante 6.0",
+                    "version": "V12 470cv",
+                    "yearFabrication": "2011",
+                    "yearModel": "2012",
+                    "kilometers": "83909",
+                    "color": "Black"
+                  }
+                }
+            """;
+
+    public static final String ADD_PRODUCT_STOCK = """
+                {
+                  "productCode": "MYW-6552",
+                  "quantity": 1
+                }
+            """;
+
+    public static final String UPDATE_PRODUCT = """
+                {
+                      "code": "MYW-6552",
+                      "unitValue": 666670.00,
+                      "category": "VEHICLE",
+                      "details": {
+                        "plate": "MYW-6552",
+                        "make": "ASTON MARTIN",
+                        "model": "DB9 Volante 6.0",
+                        "version": "V12 470cv",
+                        "yearFabrication": "2011",
+                        "yearModel": "2012",
+                        "kilometers": "88909",
+                        "color": "Black"
+                      }
+                    }
+            """;
+
+    public static final String CREATE_ORDER = """
+            {
+              "customer": "636.803.004-68",
+              "products": [
+                {
+                  "product": {
+                    "code": "MYW-6552",
+                    "unitValue": 626670.00
+                  },
+                  "quantity": 1
+                }
+              ]
+            }
+            """;
+
+}
